a1e1d459ad0295188225d6be3e4c61f5204bba46
Urbdksnfj <160196582+Urbdksnfj@users.noreply.github.com>
2024-02-19 17:21:35 +0200

    Update play.py

--- ZeMusic/plugins/play/play.py
+++ ZeMusic/plugins/play/play.py
@@ -32,7 +32,7 @@ from config import BANNED_USERS, lyrical
             "تشغيل",
             "شغل",
             "",
-            "/vplay","فديو","فيديو",
+            "فديو",
             "/cplay",
             "/cvplay",
             "/playforce",
